5fd54be4f8032ad140858a31d4d22c81e1a503f7
hamdaouiwassim <hamdaouiwassim@gmail.com>
2025-09-25 10:22:57 +0100

    fix getUserFromToken

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use Auth;
 use Hash;
 use Validator;
+use Laravel\Passport\Token;
 use App\Models\User;
 use App\Models\Media;
 use App\Models\Product;
@@ -16,28 +17,25 @@ class UserController extends BaseController
     //
 
     public function products(Request $request){
-
-      
-    
         try{
-            $user = getUserFromToken($request);
-            
-            if ($user){ 
+            $user = $this->getUserFromToken($request);
+
+            if ($user){
                 return $this->okResponse(Product::with('threedmodels','photos','rooms','metals','category','measure','measure.dimension','measure.weight','user')->get(),"Product List ...");
              }
             else { return $this->errorResponse('error',['error' => 'Can\'t found user'],404); }
         }catch(Exception $e){
             return $this->errorResponse('error',['error' => $e],500);
         }
-     
+
     }
 
     public function updateUser(Request $request){
         try{
-            $user = getUserFromToken($request);
-            
-            if ($user){ 
-             
+            $user = $this->getUserFromToken($request);
+
+            if ($user){
+
             (!empty($request->name)) && $user->name = $request->name;
            (!empty($request->country)) && $user->country = $request->country;
            (!empty($request->phone)) && $user->phone = $request->phone;
@@ -46,7 +44,7 @@ class UserController extends BaseController
             $validator = Validator::make($request->all(),[
                 "photoUrl" => "required|mimes:jpeg,png,jpg|max:2024",
             ]);
-    
+
             if ($validator->fails()){
                 return $this->errorResponse(Arr::first(Arr::flatten($validator->messages()->get('*'))),[],401);
             }
@@ -77,23 +75,24 @@ class UserController extends BaseController
         try{
             $validator = Validator::make($request->all(),[
                 "oldPass" => "required|min:8",
-                "newPass" => "required|min:8", 
+                "newPass" => "required|min:8",
             ]);
-    
+
             if ($validator->fails()){
                 return $this->errorResponse(Arr::first(Arr::flatten($validator->messages()->get('*'))),[],401);
             }
-            $user = getUserFromToken($request);
-            
-            if ($user){ 
+            $user = $this->getUserFromToken($request);
+
+
+            if ($user){
                 if ( Hash::check($request->oldPass, $user->password) ){
                     $user->password = Hash::make($request->newPass);
                     $user->update();
                     return $this->okResponse($user,"Password updated successfully ...");
                 }else{
-                    return $this->errorResponse('Old password are wrong',[],401); 
+                    return $this->errorResponse('Old password are wrong',[],401);
                 }
-                
+
              }
             else { return $this->errorResponse('Can\'t Update User',[],401); }
         }catch(Exception $e){
@@ -101,7 +100,21 @@ class UserController extends BaseController
         }
     }
 
+private function getUserFromToken($request){
+    // Passport middleware will already resolve the user if the token is valid
+    if (Auth::check()) {
+        return Auth::user();
+    }
 
-    
+    // If you want to manually parse the token from request header:
+    $token = $request->bearerToken();
+    if (!$token) {
+        return null;
+    }
+
+    $user = Auth::guard('api')->user(); // get user from passport guard
+    return $user;
+}
+
 
 }
